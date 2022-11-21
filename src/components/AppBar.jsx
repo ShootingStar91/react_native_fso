8f6aa720baf54d5448065c6573f068b0e7656238
arttu <ttu91@hotmail.com>
2022-11-21 15:39:47 +0200

    10.7 done

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,25 +1,39 @@
-import { View, StyleSheet, Pressable, Text } from 'react-native';
-import theme from './../theme';
-import Constants from 'expo-constants';
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import theme from "./../theme";
+import Constants from "expo-constants";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#24292e'
+    backgroundColor: "#24292e",
+    display: "flex",
+    paddingLeft: 10,
+    paddingBottom: 20,
+    flexDirection: "row",
   },
   header: {
     fontSize: 18,
-    color: '#ffffff',
+    color: "#ffffff",
     fontWeight: theme.fontWeights.bold,
-    marginLeft: 10,
-    marginBottom: 20
-  }
+    flexGrow: 0,
+    marginRight: 15,
+  },
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>
-    <Pressable><Text style={styles.header}>Repositories</Text></Pressable>
-  </View>;
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <Link to="/">
+          <Text style={styles.header}>Repositories</Text>
+        </Link>
+        <Link to="/signin">
+          <Text style={styles.header}>Sign in</Text>
+        </Link>
+      </ScrollView>
+    </View>
+  );
 };
 
 export default AppBar;
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -19,7 +19,7 @@ const styles = StyleSheet.create({
   header: {
     marginLeft: 15,
     display: "flex",
-    flexGrow: 0,
+    flexGrow: 0
   },
   headerItem: {
     padding: 3,
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   stats: {
     display: "flex",
     justifyContent: "center",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   stat: {
     flexGrow: 0,
@@ -48,56 +48,72 @@ const RepositoryItem = ({ item }) => {
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-        <Image style={styles.image} source={{ uri: item.ownerAvatarUrl }} />
-        <View style={styles.header}>
-          <Text fontWeight="bold" style={styles.headerItem}>
-            {item.fullName}
-          </Text>
-          <Text color="textSecondary" style={styles.headerItem}>
-            {item.description}
-          </Text>
-          <View style={styles.headerItem}>
-            <Text
-              style={{
-                backgroundColor: "blue",
-                borderRadius: 3,
-                color: "white",
-                padding: 3,
-                alignSelf: "flex-start",
-              }}
-            >
-              {item.language}
-            </Text>
-          </View>
-        </View>
+        <Header item={item} />
       </View>
       <View style={styles.stats}>
-        <View style={styles.stat}>
-          <Text fontWeight="bold" style={{ alignSelf: "center" }}>
-            {formatNumber(item.stargazersCount)}
-          </Text>
-          <Text color="textSecondary">Stars</Text>
-        </View>
-        <View style={styles.stat}>
-          <Text fontWeight="bold" style={{ alignSelf: "center" }}>
-            {formatNumber(item.forksCount)}
-          </Text>
-          <Text color="textSecondary">Forks</Text>
-        </View>
-        <View style={styles.stat}>
-          <Text fontWeight="bold" style={{ alignSelf: "center" }}>
-            {formatNumber(item.reviewCount)}
-          </Text>
-          <Text color="textSecondary">Reviews</Text>
-        </View>
-        <View style={styles.stat}>
-          <Text fontWeight="bold" style={{ alignSelf: "center" }}>
-            {formatNumber(item.ratingAverage)}
-          </Text>
-          <Text color="textSecondary">Rating</Text>
-        </View>
+        <Stats item={item} />
       </View>
     </View>
+  );
+};
+
+const Header = ({ item }) => {
+  return (
+    <>
+      <Image style={styles.image} source={{ uri: item.ownerAvatarUrl }} />
+      <View style={styles.header}>
+        <Text fontWeight="bold" style={styles.headerItem}>
+          {item.fullName}
+        </Text>
+        <Text color="textSecondary" style={{...styles.headerItem, paddingRight: 50}}>
+          {item.description}
+        </Text>
+        <View style={styles.headerItem}>
+          <Text
+            style={{
+              backgroundColor: "blue",
+              borderRadius: 3,
+              color: "white",
+              padding: 3,
+              alignSelf: "flex-start",
+            }}
+          >
+            {item.language}
+          </Text>
+        </View>
+      </View>
+    </>
+  );
+};
+
+const Stats = ({ item }) => {
+  return (
+    <>
+      <View style={styles.stat}>
+        <Text fontWeight="bold" style={{ alignSelf: "center" }}>
+          {formatNumber(item.stargazersCount)}
+        </Text>
+        <Text color="textSecondary">Stars</Text>
+      </View>
+      <View style={styles.stat}>
+        <Text fontWeight="bold" style={{ alignSelf: "center" }}>
+          {formatNumber(item.forksCount)}
+        </Text>
+        <Text color="textSecondary">Forks</Text>
+      </View>
+      <View style={styles.stat}>
+        <Text fontWeight="bold" style={{ alignSelf: "center" }}>
+          {formatNumber(item.reviewCount)}
+        </Text>
+        <Text color="textSecondary">Reviews</Text>
+      </View>
+      <View style={styles.stat}>
+        <Text fontWeight="bold" style={{ alignSelf: "center" }}>
+          {formatNumber(item.ratingAverage)}
+        </Text>
+        <Text color="textSecondary">Rating</Text>
+      </View>
+    </>
   );
 };
 
